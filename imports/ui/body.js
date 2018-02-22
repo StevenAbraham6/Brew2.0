7f0d388c2b6f78d1746bc053c1907cb94d9bf2c0
stevabra <steven4abraham@gmail.com>
2018-02-22 14:08:22 +0530

    Start UserId

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -75,4 +75,20 @@ Template.body.helpers({
    // Show newest tasks at the top
    return Tasks.find({}, { sort: { createdAt: -1 } });
  },
+
+ emails(){
+   // HTTP.call('GET', 'https://api.ciscospark.com/v1/people/me', {
+   //   data: { some: 'json', stuff: 1 }
+   // }, (error, result) => {
+   //   if (!error) {
+   //     Session.set('twizzled', true);
+   //   }
+   // });
+
+   var email=Session.get('email')
+   //response=HTTP.call( 'GET', 'https://api.ciscospark.com/v1/people/me', { headers:{ 'Content-type' : 'application/json; charset=utf-8', 'Authorization': 'Bearer NjViZGY4OTctM2EyZi00M2FhLWE3Y2YtNGFmYjBiY2M2YTM0YWEyZDcyZWItMDA2' } } );
+   //this.unblock();
+   //return response.data.emails[0];
+   return email;//FlowRouter.getParam('code')
+ },
 });
